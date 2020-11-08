de2ddfef3b405ba09b7bec5704ac24cd3b3b11a2
laila2903 <43816009+laila2903@users.noreply.github.com>
2020-11-08 11:04:25 -0500

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World Jenkin!");
-        System.out.println("tested 08.11.20");
+        System.out.println("tested 08.11.20 klokka 17:04");
     }
 }
